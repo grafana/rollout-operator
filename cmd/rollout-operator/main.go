195e148e09cc9b9f6fff5635c9b569886c896d76
Marco Pracucci <marco@pracucci.com>
2021-07-15 09:17:59 +0200

    Rename to rollout-operator (#2)
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- cmd/rollout-operator/main.go
+++ cmd/rollout-operator/main.go
@@ -12,7 +12,7 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 
-	"github.com/grafana/cortex-operator/pkg/controller"
+	"github.com/grafana/rollout-operator/pkg/controller"
 
 	// Required to get the GCP auth provider working.
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/grafana/cortex-operator
+module github.com/grafana/rollout-operator
 
 go 1.16
 
