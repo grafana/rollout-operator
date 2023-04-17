80c3f109ae718e3b0427afef04aecfac07619848
Michel Hollands <michel.hollands@grafana.com>
2023-04-17 16:53:45 +0100

    Return early in case of dry run
    Signed-off-by: Michel Hollands <michel.hollands@grafana.com>

--- pkg/admission/prep_downscale.go
+++ pkg/admission/prep_downscale.go
@@ -130,82 +130,82 @@ func prepDownscale(ctx context.Context, logger log.Logger, ar v1.AdmissionReview
 		return &reviewResponse
 	}
 
-	// Since it's a downscale, check if the resource has the label that indicates it needs to be prepared to be downscaled.
-	// Create a slice of endpoint addresses for pods to send HTTP post requests to and to fail if any don't return 200
-	if !*ar.Request.DryRun {
-		diff := (*oldReplicas - *newReplicas)
-		type endpoint struct {
-			url   string
-			index int
-		}
-		eps := make([]endpoint, diff)
-
-		// The DNS entry for a pod of a stateful set is
-		// ingester-zone-a-0.$(servicename).$(namespace).svc.cluster.local
-		// The service in this case is ingester-zone-a as well.
-		// https://kubernetes.io/docs/concepts/workloads/controllers/statefulset/#stable-network-id
-		for i := 0; i < int(diff); i++ {
-			index := int(*oldReplicas) - i - 1 // nr in statefulset
-			eps[i].url = fmt.Sprintf("%v-%v.%v.%v.svc.cluster.local:%s/ingester/%s",
-				ar.Request.Name, // pod name
-				index,
-				ar.Request.Name, // svc name
-				ar.Request.Namespace,
-				port,
-				path,
-			)
-			eps[i].index = index
-		}
-
-		g, _ := errgroup.WithContext(ctx)
-		for _, ep := range eps {
-			ep := ep // https://golang.org/doc/faq#closures_and_goroutines
-			g.Go(func() error {
-				logger = log.With(logger, "url", ep.url, "index", ep.index)
-				err = addPreparedForDownscaleAnnotationToPod(ctx, api, ar.Request.Namespace, ar.Request.Name, ep.index)
-				if err != nil {
-					level.Debug(logger).Log("msg", "adding annotation to pod failed", "err", err)
-				}
-				level.Debug(logger).Log("msg", "annotation added to pod")
-
-				resp, err := client.Post("http://"+ep.url, "application/json", nil)
-				if err != nil {
-					level.Error(logger).Log("msg", "error sending HTTP post request", "err", err)
-					return err
-				}
-				if resp.StatusCode/100 != 2 {
-					err := errors.New("HTTP post request returned non-2xx status code")
-					body, readError := io.ReadAll(resp.Body)
-					defer resp.Body.Close()
-					level.Error(logger).Log("msg", err, "status", resp.StatusCode, "response_body", body)
-					return errors.Join(err, readError)
-				}
-				level.Debug(logger).Log("msg", "pod prepared for shutdown")
-				return nil
-			})
-		}
-		err := g.Wait()
-		if err != nil {
-			// Down-scale operation is disallowed because a pod failed to prepare for shutdown and cannot be deleted
-			reviewResponse := v1.AdmissionResponse{
-				Allowed: false,
-				Result: &metav1.Status{
-					Message: fmt.Sprintf("downscale of %s/%s in %s from %d to %d replicas is not allowed because one or more pods failed to prepare for shutdown.", ar.Request.Resource.Resource, ar.Request.Name, ar.Request.Namespace, *oldReplicas, *newReplicas),
-				},
-			}
-			level.Warn(logger).Log("msg", "downscale not allowed")
-			return &reviewResponse
-		}
-
-		// Down-scale operation is allowed because all pods successfully prepared for shutdown
-		level.Info(logger).Log("msg", "downscale allowed")
-		return &v1.AdmissionResponse{
-			Allowed: true,
-			Result: &metav1.Status{
-				Message: fmt.Sprintf("downscale of %s/%s in %s from %d to %d replicas is allowed -- all pods successfully prepared for shutdown.", ar.Request.Resource.Resource, ar.Request.Name, ar.Request.Namespace, *oldReplicas, *newReplicas),
-			},
-		}
+	if *ar.Request.DryRun {
+		return &v1.AdmissionResponse{Allowed: true}
 	}
 
-	return &v1.AdmissionResponse{Allowed: true}
+	// Since it's a downscale, check if the resource has the label that indicates it needs to be prepared to be downscaled.
+	// Create a slice of endpoint addresses for pods to send HTTP post requests to and to fail if any don't return 200
+	diff := (*oldReplicas - *newReplicas)
+	type endpoint struct {
+		url   string
+		index int
+	}
+	eps := make([]endpoint, diff)
+
+	// The DNS entry for a pod of a stateful set is
+	// ingester-zone-a-0.$(servicename).$(namespace).svc.cluster.local
+	// The service in this case is ingester-zone-a as well.
+	// https://kubernetes.io/docs/concepts/workloads/controllers/statefulset/#stable-network-id
+	for i := 0; i < int(diff); i++ {
+		index := int(*oldReplicas) - i - 1 // nr in statefulset
+		eps[i].url = fmt.Sprintf("%v-%v.%v.%v.svc.cluster.local:%s/ingester/%s",
+			ar.Request.Name, // pod name
+			index,
+			ar.Request.Name, // svc name
+			ar.Request.Namespace,
+			port,
+			path,
+		)
+		eps[i].index = index
+	}
+
+	g, _ := errgroup.WithContext(ctx)
+	for _, ep := range eps {
+		ep := ep // https://golang.org/doc/faq#closures_and_goroutines
+		g.Go(func() error {
+			logger = log.With(logger, "url", ep.url, "index", ep.index)
+			err = addPreparedForDownscaleAnnotationToPod(ctx, api, ar.Request.Namespace, ar.Request.Name, ep.index)
+			if err != nil {
+				level.Debug(logger).Log("msg", "adding annotation to pod failed", "err", err)
+			}
+			level.Debug(logger).Log("msg", "annotation added to pod")
+
+			resp, err := client.Post("http://"+ep.url, "application/json", nil)
+			if err != nil {
+				level.Error(logger).Log("msg", "error sending HTTP post request", "err", err)
+				return err
+			}
+			if resp.StatusCode/100 != 2 {
+				err := errors.New("HTTP post request returned non-2xx status code")
+				body, readError := io.ReadAll(resp.Body)
+				defer resp.Body.Close()
+				level.Error(logger).Log("msg", err, "status", resp.StatusCode, "response_body", body)
+				return errors.Join(err, readError)
+			}
+			level.Debug(logger).Log("msg", "pod prepared for shutdown")
+			return nil
+		})
+	}
+	err = g.Wait()
+	if err != nil {
+		// Down-scale operation is disallowed because a pod failed to prepare for shutdown and cannot be deleted
+		reviewResponse := v1.AdmissionResponse{
+			Allowed: false,
+			Result: &metav1.Status{
+				Message: fmt.Sprintf("downscale of %s/%s in %s from %d to %d replicas is not allowed because one or more pods failed to prepare for shutdown.", ar.Request.Resource.Resource, ar.Request.Name, ar.Request.Namespace, *oldReplicas, *newReplicas),
+			},
+		}
+		level.Warn(logger).Log("msg", "downscale not allowed")
+		return &reviewResponse
+	}
+
+	// Down-scale operation is allowed because all pods successfully prepared for shutdown
+	level.Info(logger).Log("msg", "downscale allowed")
+	return &v1.AdmissionResponse{
+		Allowed: true,
+		Result: &metav1.Status{
+			Message: fmt.Sprintf("downscale of %s/%s in %s from %d to %d replicas is allowed -- all pods successfully prepared for shutdown.", ar.Request.Resource.Resource, ar.Request.Name, ar.Request.Namespace, *oldReplicas, *newReplicas),
+		},
+	}
 }
