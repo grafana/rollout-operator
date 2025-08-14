b878c71aa16e4ab973e298615307826a51d3ce19
Andrew Hall <andrew.hall@grafana.com>
2025-08-14 10:46:03 +0800

    WIP integrations

--- integration/manifests_rollout_operator_test.go
+++ integration/manifests_rollout_operator_test.go
@@ -12,6 +12,8 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes"
@@ -19,14 +21,14 @@ import (
 
 const certificateSecretName = "certificate"
 
-func createRolloutOperator(t *testing.T, ctx context.Context, api *kubernetes.Clientset, namespace string, webhook bool) {
-	createRolloutOperatorDependencies(t, ctx, api, namespace, webhook)
+func createRolloutOperator(t *testing.T, ctx context.Context, api *kubernetes.Clientset, extApi *apiextensionsclient.Clientset, namespace string, webhook bool) {
+	createRolloutOperatorDependencies(t, ctx, api, extApi, namespace, webhook)
 
 	_, err := api.AppsV1().Deployments(namespace).Create(ctx, rolloutOperatorDeployment(namespace, webhook), metav1.CreateOptions{})
 	require.NoError(t, err)
 }
 
-func createRolloutOperatorDependencies(t *testing.T, ctx context.Context, api *kubernetes.Clientset, namespace string, webhook bool) {
+func createRolloutOperatorDependencies(t *testing.T, ctx context.Context, api *kubernetes.Clientset, extApi *apiextensionsclient.Clientset, namespace string, webhook bool) {
 	_, err := api.CoreV1().ServiceAccounts(namespace).Create(ctx, rolloutOperatorServiceAccount(), metav1.CreateOptions{})
 	require.NoError(t, err)
 
@@ -34,6 +36,10 @@ func createRolloutOperatorDependencies(t *testing.T, ctx context.Context, api *k
 	require.NoError(t, err)
 
 	_, err = api.RbacV1().RoleBindings(namespace).Create(ctx, rolloutOperatorRoleBinding(namespace), metav1.CreateOptions{})
+	require.NoError(t, err)
+
+	_, err = extApi.ApiextensionsV1().CustomResourceDefinitions().
+		Create(context.Background(), zoneAwarePodDistruptionBudgetCustomResourceDefinition(), metav1.CreateOptions{})
 	require.NoError(t, err)
 
 	if webhook {
@@ -285,6 +291,173 @@ func webhookRolloutOperatorClusterRole(namespace string) *rbacv1.ClusterRole {
 				APIGroups: []string{"admissionregistration.k8s.io"},
 				Resources: []string{"validatingwebhookconfigurations", "mutatingwebhookconfigurations"},
 				Verbs:     []string{"list", "patch", "watch"},
+			},
+		},
+	}
+}
+
+func zoneAwarePodDistruptionBudgetCustomResourceDefinition() *apiextensionsv1.CustomResourceDefinition {
+	return &apiextensionsv1.CustomResourceDefinition{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "zoneawarepoddisruptionbudgets.rollout-operator.grafana.com",
+		},
+		Spec: apiextensionsv1.CustomResourceDefinitionSpec{
+			Group: "rollout-operator.grafana.com",
+			Versions: []apiextensionsv1.CustomResourceDefinitionVersion{
+				{
+					Name:    "v1",
+					Served:  true,
+					Storage: true,
+					Schema: &apiextensionsv1.CustomResourceValidation{
+						OpenAPIV3Schema: &apiextensionsv1.JSONSchemaProps{
+							Type: "object",
+							Properties: map[string]apiextensionsv1.JSONSchemaProps{
+								"spec": {
+									Type:     "object",
+									Required: []string{"selector"},
+									Properties: map[string]apiextensionsv1.JSONSchemaProps{
+										"maxUnavailable": {
+											Type:        "integer",
+											Description: "The number of pods that can be unavailable within a zone or partition.",
+											Minimum:     &[]float64{0}[0],
+										},
+										"maxUnavailablePercentage": {
+											Type:        "integer",
+											Description: "Calculate the maxUnavailable value as a percentage of the StatefulSet's spec.Replica count. This option is not supported when using podNamePartitionRegex.",
+											Minimum:     &[]float64{0}[0],
+											Maximum:     &[]float64{100}[0],
+										},
+										"selector": {
+											Type:        "object",
+											Description: "A selector for finding pods and statefulsets that this ZoneAwarePodDisruptionBudget applies to.",
+											Required:    []string{"matchLabels"},
+											Properties: map[string]apiextensionsv1.JSONSchemaProps{
+												"matchLabels": {
+													Type:                 "object",
+													AdditionalProperties: &apiextensionsv1.JSONSchemaPropsOrBool{Schema: &apiextensionsv1.JSONSchemaProps{Type: "string"}},
+												},
+											},
+										},
+										"podNamePartitionRegex": {
+											Type:        "string",
+											Description: "A regular expression for returning a partition name given a pod name. This field is optional and should only be used when the ZoneAwarePodDisruptionBudget is to be scoped to a partition, such as a multi-zone ingester deployment with ingest_storage_enabled. Enabling this changes the ZPDB functionality such that minAvailability is applied across ALL zones for a given partition. When not enabled, the minAvailability is applied to pods within the eviction zone assuming there are no disruptions in the other zones.",
+										},
+										"podNameRegexGroup": {
+											Type:        "integer",
+											Minimum:     &[]float64{1}[0],
+											Description: "The regular expression group number that contains the partition name. This field is only required when the podNamePartitionRegex field is set and has more then one subexpression grouping. The default value is 1.",
+										},
+									},
+								},
+							},
+						},
+					},
+					Subresources: &apiextensionsv1.CustomResourceSubresources{
+						Status: &apiextensionsv1.CustomResourceSubresourceStatus{},
+					},
+				},
+			},
+			Scope: apiextensionsv1.NamespaceScoped,
+			Names: apiextensionsv1.CustomResourceDefinitionNames{
+				Kind:       "ZoneAwarePodDisruptionBudget",
+				Plural:     "zoneawarepoddisruptionbudgets",
+				Singular:   "zoneawarepoddisruptionbudget",
+				ShortNames: []string{"pdbz"},
+			},
+		},
+	}
+}
+
+func zpdbValidatingWebhook(namespace string) *admissionregistrationv1.ValidatingWebhookConfiguration {
+	name := "zpdb-validation-" + namespace
+
+	return &admissionregistrationv1.ValidatingWebhookConfiguration{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+			Labels: map[string]string{
+				"grafana.com/inject-rollout-operator-ca": "true",
+				"grafana.com/namespace":                  namespace,
+			},
+		},
+		Webhooks: []admissionregistrationv1.ValidatingWebhook{
+			{
+				Name: name + ".grafana.com",
+				ClientConfig: admissionregistrationv1.WebhookClientConfig{
+					Service: &admissionregistrationv1.ServiceReference{
+						Namespace: namespace,
+						Name:      "rollout-operator",
+						Path:      ptr("/admission/zpdb-validation"),
+					},
+				},
+				Rules: []admissionregistrationv1.RuleWithOperations{
+					{
+						Operations: []admissionregistrationv1.OperationType{
+							admissionregistrationv1.Create,
+							admissionregistrationv1.Update,
+						},
+						Rule: admissionregistrationv1.Rule{
+							APIGroups:   []string{"rollout-operator.grafana.com"},
+							APIVersions: []string{"v1"},
+							Resources:   []string{"zoneawarepoddisruptionbudgets"},
+							Scope:       ptr(admissionregistrationv1.NamespacedScope),
+						},
+					},
+				},
+				AdmissionReviewVersions: []string{"v1"},
+				NamespaceSelector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"kubernetes.io/metadata.name": namespace,
+					},
+				},
+				FailurePolicy: ptr(admissionregistrationv1.Fail),
+				SideEffects:   ptr(admissionregistrationv1.SideEffectClassNone),
+			},
+		},
+	}
+}
+
+func podEvictionValidatingWebhook(namespace string) *admissionregistrationv1.ValidatingWebhookConfiguration {
+	name := "pod-eviction-" + namespace
+
+	return &admissionregistrationv1.ValidatingWebhookConfiguration{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+			Labels: map[string]string{
+				"grafana.com/inject-rollout-operator-ca": "true",
+				"grafana.com/namespace":                  namespace,
+			},
+		},
+		Webhooks: []admissionregistrationv1.ValidatingWebhook{
+			{
+				Name: name + ".grafana.com",
+				ClientConfig: admissionregistrationv1.WebhookClientConfig{
+					Service: &admissionregistrationv1.ServiceReference{
+						Namespace: namespace,
+						Name:      "rollout-operator",
+						Path:      ptr("/admission/pod-eviction"),
+					},
+				},
+				Rules: []admissionregistrationv1.RuleWithOperations{
+					{
+						Operations: []admissionregistrationv1.OperationType{
+							admissionregistrationv1.Create,
+						},
+						Rule: admissionregistrationv1.Rule{
+							APIGroups:   []string{""},
+							APIVersions: []string{"v1"},
+							Resources:   []string{"pods/eviction"},
+							Scope:       ptr(admissionregistrationv1.NamespacedScope),
+						},
+					},
+				},
+				AdmissionReviewVersions: []string{"v1"},
+				NamespaceSelector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"kubernetes.io/metadata.name": namespace,
+					},
+				},
+				FailurePolicy: ptr(admissionregistrationv1.Fail),
+				SideEffects:   ptr(admissionregistrationv1.SideEffectClassNone),
 			},
 		},
 	}
