2c648da56edc976aebe66f3f710d59077a8fc679
Segflow <asselmeher@gmail.com>
2025-12-09 22:19:56 +0100

    add comment about callPrepareDownscaleAndReturnElapsedDurationsSinceInitiatedDownscale

--- pkg/controller/delay.go
+++ pkg/controller/delay.go
@@ -200,6 +200,8 @@ func callPrepareDownscaleAndReturnElapsedDurationsSinceInitiatedDownscale(ctx co
 			}
 
 			if resp.StatusCode/100 != 2 {
+				// Unlike `callCancelDelayedDownscale`, here we consider all non 2xx status code as error and should block the downscale.
+				// this includes the case where we fail because a partition state change is locked.
 				level.Error(epLogger).Log("msg", "unexpected status code returned when calling DELETE on endpoint", "status", resp.StatusCode, "response_body", string(body))
 				return fmt.Errorf("HTTP DELETE request returned non-2xx status code: %v", resp.StatusCode)
 			}
@@ -262,7 +264,7 @@ func callCancelDelayedDownscale(ctx context.Context, logger log.Logger, client h
 			if resp.StatusCode/100 != 2 {
 				body, readError := io.ReadAll(resp.Body)
 
-				// Handle 409 Conflict separately - this typically means partition state is locked by an operator
+				// Handle 409 Conflict separately - this typically means that the partition state is locked by an engineer
 				if resp.StatusCode == http.StatusConflict {
 					level.Info(epLogger).Log("msg", "HTTP DELETE request returned 409 status code, delayed downscale cancellation skipped", "status", resp.StatusCode, "response_body", string(body))
 					return nil
