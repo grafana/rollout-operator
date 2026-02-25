f293ecfef4626b227891677671715e13905cb4d1
Daniel Lawless <danlawless1999@gmail.com>
2026-02-25 13:54:39 +1000

    Add -kubernetes.client-qps flag for kube client rate limiter

--- cmd/rollout-operator/main.go
+++ cmd/rollout-operator/main.go
@@ -57,6 +57,7 @@ type config struct {
 	kubeClusterDomain    string
 	kubeNamespace        string
 	kubeClientTimeout    time.Duration
+	kubeClientQPS        float64
 	reconcileInterval    time.Duration
 	clusterValidationCfg clusterutil.ClusterValidationProtocolConfigForHTTP
 
@@ -84,6 +85,7 @@ func (cfg *config) register(fs *flag.FlagSet) {
 	fs.StringVar(&cfg.kubeAPIURL, "kubernetes.api-url", "", "The Kubernetes server API URL. If not specified, it will be auto-detected when running within a Kubernetes cluster.")
 	fs.StringVar(&cfg.kubeConfigFile, "kubernetes.config-file", "", "The Kubernetes config file path. If not specified, it will be auto-detected when running within a Kubernetes cluster.")
 	fs.DurationVar(&cfg.kubeClientTimeout, "kubernetes.client-timeout", 5*time.Minute, "HTTP client timeout. This applies to requests issued to both the Kubernetes API and Kubernetes resource endpoints.")
+	fs.Float64Var(&cfg.kubeClientQPS, "kubernetes.client-qps", 0, "Maximum QPS to the API server from this client. If zero, the client-go default (5) is used.")
 	fs.StringVar(&cfg.kubeClusterDomain, "kubernetes.cluster-domain", "cluster.local.", "The Kubernetes cluster domain.")
 	fs.StringVar(&cfg.kubeNamespace, "kubernetes.namespace", "", "The Kubernetes namespace for which this operator is running.")
 	fs.DurationVar(&cfg.reconcileInterval, "reconcile.interval", 5*time.Second, "The minimum interval of reconciliation.")
@@ -179,6 +181,12 @@ func main() {
 
 	if kubeConfig.Timeout == 0 {
 		kubeConfig.Timeout = cfg.kubeClientTimeout
+	}
+	if cfg.kubeClientQPS > 0 {
+		kubeConfig.QPS = float32(cfg.kubeClientQPS)
+		if kubeConfig.Burst == 0 {
+			kubeConfig.Burst = 10
+		}
 	}
 	if kubeConfig.UserAgent == "" {
 		kubeConfig.UserAgent = rest.DefaultKubernetesUserAgent()
